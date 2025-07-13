eb13050072af81bad2d38747a1a3e92c37930840
Dibyajit Nandi <161131517+dibyajit1234@users.noreply.github.com>
2025-07-13 18:57:21 +0000

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
     // alert(User); 
     console.log(User)
     try {
-      const response =await fetch(`http://localhost:5000/api/auth/register`,{
+      const response =await fetch(`https://first-mern-backend-3jkq.onrender.com/api/auth/register`,{
       method:"POST",
       headers:{
         "Content-Type":"application/json",
