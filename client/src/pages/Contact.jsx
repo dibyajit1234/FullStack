3fdf460f7d6a04c37adc5360379f9e846275e067
Dibyajit Nandi <161131517+dibyajit1234@users.noreply.github.com>
2025-07-13 18:55:36 +0000

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
       // alert(Contact); 
       // console.log(Contact)
       try {
-        const response = await fetch(`http://localhost:5000/api/form/contact`,{
+        const response = await fetch(`https://first-mern-backend-3jkq.onrender.com/api/form/contact`,{
           method:"POST",
           headers:{
             "Content-Type":"application/json",
