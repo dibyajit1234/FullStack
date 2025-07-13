5f8ed3048094607279724b50a4b60a7f3a925358
dibyajit1234 <dibyajitnandi7205@gmail.com>
2025-07-14 00:54:00 +0530

    ready

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const adminRouter = require("./router/admin-router")
 const path = require("path")
 const cors = require('cors')
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://first-mern-frontent.onrender.com',
     methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
     credential:true
 }
